d2c5f5a85a006db0d6c1e843640f7b9e1ba6199b
Akhil <akhil@Akhils-MacBook-Pro.local>
2019-03-11 00:05:51 +0530

    Added create and update methods

--- src/main.go
+++ src/main.go
@@ -43,9 +43,38 @@ func getBook(writer http.ResponseWriter, request *http.Request) {
 }
 
 func deleteBook(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Deleted book successfully."))
+	parameters := mux.Vars(request)
+	for index, item := range books {
+		if item.ID == parameters["id"] {
+			books = append(books[0:index], books[index+1:]...)
+			writer.Write([]byte("Deleted book successfully."))
+			return
+		}
+	}
 	writer.Write([]byte("Id not found"))
+}
 
+func createBook(writer http.ResponseWriter, request *http.Request) {
+	var book Book
+	_ = json.NewDecoder(request.Body).Decode(&book)
+	book.ID = "808"
+	books = append(books, book)
+	json.NewEncoder(writer).Encode(book)
+}
+
+func updateBook(writer http.ResponseWriter, request *http.Request) {
+	parameters := mux.Vars(request)
+	var book Book
+	_ = json.NewDecoder(request.Body).Decode(&book)
+	for index, item := range books {
+		if item.ID == parameters["id"] {
+			book.ID = item.ID
+			books = append(books[0:index], books[index+1:]...)
+			books = append(books, book)
+			writer.Write([]byte("Updated book successfully."))
+			return
+		}
+	}
 }
 
 func main() {
@@ -64,8 +93,10 @@ func main() {
 	fmt.Println("Hi there", books)
 	router := mux.NewRouter()
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
-	router.HandleFunc("/api/books?id={id}", getBook).Methods("GET")
+	router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	router.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/api/books", createBook).Methods("POST")
+	router.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
